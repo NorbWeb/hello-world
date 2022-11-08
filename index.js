9890b25e45999ffd5555247a589fa1f67f1d0b8a
Norbert Madauß <entwickler@delphi-imm.de>
2022-11-08 12:42:49 +0100

    fix

--- index.js
+++ index.js
@@ -4,20 +4,3 @@ function myFunction() {
 }
 
 
-// window.addEventListener('load', () => {
-//   registerSW();
-// });
-
-// // Register the Service Worker
-// async function registerSW() {
-//   if ('serviceWorker' in navigator) {
-//     try {
-//       await navigator
-//             .serviceWorker
-//             .register('serviceworker.js');
-//     }
-//     catch (e) {
-//       console.log('SW registration failed');
-//     }
-//   }
-// }
