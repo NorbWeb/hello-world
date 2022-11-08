6622933aa4a02895c4f9c4430f10330bcffefff6
Norbert Madauß <entwickler@delphi-imm.de>
2022-11-08 15:16:50 +0100

    new try for serviceworker

--- serviceworker.js
+++ serviceworker.js
@@ -1,19 +1,42 @@
-var staticCacheName = "pwa";
+// Change this to your repository name
+var GHPATH = '/hello-world';
  
-self.addEventListener("install", function (e) {
-  e.waitUntil(
-    caches.open(staticCacheName).then(function (cache) {
-      return cache.addAll(["/"]);
-    })
-  );
-});
+// Choose a different app prefix name
+var APP_PREFIX = 'hw_';
  
-self.addEventListener("fetch", function (event) {
-  console.log(event.request.url);
+// The version of the cache. Every time you change any of the files
+// you need to change this version (version_01, version_02…). 
+// If you don't change the version, the service worker will give your
+// users the old files!
+var VERSION = 'version_00';
  
-  event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
-  );
-});
+// The files to make available for offline use. make sure to add 
+// others to this list
+var URLS = [    
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/style.css`,
+  `${GHPATH}/index.js`
+]
+
+
+
+// var staticCacheName = "pwa";
+ 
+// self.addEventListener("install", function (e) {
+//   e.waitUntil(
+//     caches.open(staticCacheName).then(function (cache) {
+//       return cache.addAll(["/"]);
+//     })
+//   );
+// });
+ 
+// self.addEventListener("fetch", function (event) {
+//   console.log(event.request.url);
+ 
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
